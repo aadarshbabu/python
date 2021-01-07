905f562be0f3759d27c41ad89e5d0fd03ad004fa
Aadarsh <aadarshsingh121@gmail.com>
2021-01-07 16:02:55 +0530

    firstprogrampython

---
+++ hello.py
@@ -0,0 +1,16 @@
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+a= 55
+b= 22
+c=a+b
+print(c)
+print(c)
+c=55
+print(c)
+print(c)
+c=11
+print(c)
